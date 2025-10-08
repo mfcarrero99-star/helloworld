196d1ece8ac4c9ba114e960bdac56ed64f9fae14
m.fcarrero99-star <m.fcarrero99@github.com>
2025-10-08 17:06:50 +0200

    Primer commit

---
+++ src/main/java/com/accenture/juego/Main.java
@@ -0,0 +1,11 @@
+package com.accenture.juego;
+
+//clase Main para implementación de juego. No sé por qué no funciona el programa porque me está dando un error del tipo 
+//mainclassnotfound. 
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/accenture/juego/Partida.java
@@ -0,0 +1,5 @@
+package com.accenture.juego;
+
+public class Partida {
+    
+}
