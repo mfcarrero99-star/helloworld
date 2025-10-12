da66be976aa2c98a3c9e664d22cec2c3b47c8423
mfcarrero99-star <m.fcarrero99@gmail.com>
2025-10-12 20:18:04 +0200

    Update Estado.java

--- src/main/java/com/accenture/juego/Estado.java
+++ src/main/java/com/accenture/juego/Estado.java
@@ -2,8 +2,8 @@ package com.accenture.juego;
 /*
  * Uso de un enum para Estado. ¿Por qué un enum, y no otra cosa como una lista?
  * Porque un enum solo puedo instanciarlo desde la clase que lo implementa. De esta 
- * manera solo puedo tener un objeto de tipo Estado en Partida. Podría jugar con varios 
- * tipo de estados, por ejemplo, EstadodePartida y EstadodeJuego. 
+ * manera solo puedo tener un objeto de tipo Estado en Partida. Hemos decidido no usar
+ el estado GUARDADO Y que siempre se guarde automaticamente la última partida. 
  */
 public enum Estado {
     EN_JUEGO,
