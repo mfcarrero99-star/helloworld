19078cae0e459aededc8033efc3018f6c5e6b0d7
mfcarrero99-star <m.fcarrero99@gmail.com>
2025-10-12 20:13:18 +0200

    Update Users.java

--- src/main/java/com/accenture/juego/Users.java
+++ src/main/java/com/accenture/juego/Users.java
@@ -1,5 +1,19 @@
 package com.accenture.juego;
 
 public class Users {
-    
+    private String nombre
+    private Partida ultimaPartida
+
+    public User(String nombre){ //constructor de usuario
+        this.nombre = nombre
+    }
+    public String getUser(){ //getter de Usuario
+        return nombre;
+    }
+    public Partida getUltimaPartida(){ //getter de la ultima partida del usuario 
+        return ultimaPartida;
+    }
+    public Partida setUltimaPartida(){ //setter de la ultima partida del usuario, siempre por default a la partida actual 
+        this.ultimaPartida = partida
+    }
 }
