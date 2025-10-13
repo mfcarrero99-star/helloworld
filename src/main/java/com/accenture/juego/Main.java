0e972c98041eefd8bcc44504b075bf93e49e9c57
m.fcarrero99-star <m.fcarrero99@github.com>
2025-10-13 10:53:23 +0200

    Nuevo commit, logica de juego simplificada

--- src/main/java/com/accenture/juego/Main.java
+++ src/main/java/com/accenture/juego/Main.java
@@ -1,7 +1,6 @@
 package com.accenture.juego;
 import java.util.Scanner;
 
-//interfaz gameable
 public class Main
 {
     public static void main( String[] args ){
--- src/main/java/com/accenture/juego/Users.java
+++ src/main/java/com/accenture/juego/Users.java
@@ -2,7 +2,6 @@ package com.accenture.juego;
 
 public class Users {
     private String user;
-    private Partida ultimaPartida;
 
     public Users(String user){ //constructor de usuario
         this.user = user;
