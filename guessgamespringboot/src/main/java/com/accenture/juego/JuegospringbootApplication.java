5ee8fcd2551e6b14c0e9753a3a1c9291b031175e
m.fcarrero99-star <m.fcarrero99@github.com>
2025-10-18 22:56:14 +0200

    Añadido proyecto de SpringBoot

---
+++ guessgamespringboot/src/main/java/com/accenture/juego/JuegospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.juego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegospringbootApplication.class, args);
+	}
+
+}
