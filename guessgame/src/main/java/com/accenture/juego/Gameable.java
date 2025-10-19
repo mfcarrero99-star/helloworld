8990090e7fa3b996d645cc0de83df582324adeb0
mfcarrero99-star <m.fcarrero99@gmail.com>
2025-10-19 09:54:35 +0200

    Update Gameable, eliminado startmenu.java

--- guessgame/src/main/java/com/accenture/juego/Gameable.java
+++ guessgame/src/main/java/com/accenture/juego/Gameable.java
@@ -2,7 +2,6 @@ package com.accenture.juego;
 import java.util.Scanner;
 //interfaz para implementar un juego que obliga a usar metodos de abrir y cerrar juego. 
 public interface Gameable{
-   void startMenu(User user, Scanner scanner);
    void startGame();
    void closeGame();
    String getGameName();
